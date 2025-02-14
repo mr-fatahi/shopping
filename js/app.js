50e3a002cb1b758ead43c11f365c29704a798dd1
Alireza Fatahi <afatahi201580@gmail.com>
2025-02-14 18:31:56 +0330

    adding modal...

--- js/app.js
+++ js/app.js
@@ -1,70 +1,75 @@
-const shoppingDB = [
-  {
-    id: 1,
-    name: "سرویس خواب دو نفره آرتا",
-    price: 3_290_000,
-    imageUrl: "/public/images/arta-s-two-3290.jpg",
-  },
-  {
-    id: 2,
-    name: "تخت خواب دو نفره فلورا",
-    price: 4_090_000,
-    imageUrl: "/public/images/flora-t-two-4090.jpg",
-  },
-  {
-    id: 3,
-    name: "تخت خواب دو طبقه رادو",
-    price: 5_290_000,
-    imageUrl: "/public/images/rado-2t-5290.jpg",
-  },
-  {
-    id: 4,
-    name: "سرویس خواب دو نفره راموناس",
-    price: 23_200_000,
-    imageUrl: "/public/images/ramonas-s-two-23200.jpg",
-  },
-  {
-    id: 5,
-    name: "سرویس خواب دو نفره روما",
-    price: 2_890_000,
-    imageUrl: "/public/images/rooma-s-two-2890.jpg",
-  },
-  {
-    id: 6,
-    name: "تخت خواب یک نفره رویا",
-    price: 2_850_000,
-    imageUrl: "/public/images/roya-t-one-2850.jpg",
-  },
-  {
-    id: 7,
-    name: "تخت خواب یک نفره صبا",
-    price: 1_845_000,
-    imageUrl: "/public/images/saba-t-one-1845.jpg",
-  },
-  {
-    id: 8,
-    name: "تخت خواب دو نفره سناتور",
-    price: 2_950_000,
-    imageUrl: "/public/images/senator-t-two-2950.jpg",
-  },
-  {
-    id: 9,
-    name: "سرویس خواب دو نفره ویدا",
-    price: 3_800_000,
-    imageUrl: "/public/images/vida-s-two-3800.jpg",
-  },
-];
 let shoppingCarts = JSON.parse(localStorage.getItem("carts")) || [];
 const cartsNumber = document.querySelector(".number");
 const carts = document.querySelector(".carts");
+const openModalBtn = document.querySelector(".header__cart");
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+function openModal() {
+  backdrop.classList.remove("hidden");
+}
+function closeModal() {
+  backdrop.classList.add("hidden");
+}
 class Shopping {
-  #db;
+  #shoppingDB = [
+    {
+      id: 1,
+      name: "سرویس خواب دو نفره آرتا",
+      price: 3_290_000,
+      imageUrl: "/public/images/arta-s-two-3290.jpg",
+    },
+    {
+      id: 2,
+      name: "تخت خواب دو نفره فلورا",
+      price: 4_090_000,
+      imageUrl: "/public/images/flora-t-two-4090.jpg",
+    },
+    {
+      id: 3,
+      name: "تخت خواب دو طبقه رادو",
+      price: 5_290_000,
+      imageUrl: "/public/images/rado-2t-5290.jpg",
+    },
+    {
+      id: 4,
+      name: "سرویس خواب دو نفره راموناس",
+      price: 23_200_000,
+      imageUrl: "/public/images/ramonas-s-two-23200.jpg",
+    },
+    {
+      id: 5,
+      name: "سرویس خواب دو نفره روما",
+      price: 2_890_000,
+      imageUrl: "/public/images/rooma-s-two-2890.jpg",
+    },
+    {
+      id: 6,
+      name: "تخت خواب یک نفره رویا",
+      price: 2_850_000,
+      imageUrl: "/public/images/roya-t-one-2850.jpg",
+    },
+    {
+      id: 7,
+      name: "تخت خواب یک نفره صبا",
+      price: 1_845_000,
+      imageUrl: "/public/images/saba-t-one-1845.jpg",
+    },
+    {
+      id: 8,
+      name: "تخت خواب دو نفره سناتور",
+      price: 2_950_000,
+      imageUrl: "/public/images/senator-t-two-2950.jpg",
+    },
+    {
+      id: 9,
+      name: "سرویس خواب دو نفره ویدا",
+      price: 3_800_000,
+      imageUrl: "/public/images/vida-s-two-3800.jpg",
+    },
+  ];
   #result = "";
-  constructor(_db) {
-    this.#db = _db;
-  }
   renderProducts() {
-    this.#db.forEach((product) => {
+    this.#shoppingDB.forEach((product) => {
       this.#result += `<section class="cart">
           <figure class="cart__image">
             <img
@@ -88,13 +93,23 @@ class Shopping {
     addToCartsBtns.forEach((btn) =>
       btn.addEventListener("click", (e) => {
         const id = Number(e.target.dataset.id);
-        const product = this.#db.find(p => p.id === id);
+        const product = this.#shoppingDB.find((p) => p.id === id);
         this.#addToCarts(product);
+        btn.textContent = "به سبد خرید افزوده شده";
+        btn.disabled = true;
       })
     );
+    this.#checkCarts(addToCartsBtns);
   }
-  #addToCarts(product){
-
+  #checkCarts(buttons) {
+    buttons.forEach((btn) => {
+      if (shoppingCarts.some((cart) => cart.id === Number(btn.dataset.id))) {
+        btn.textContent = "به سبد خرید افزوده شده";
+        btn.disabled = true;
+      }
+    });
+  }
+  #addToCarts(product) {
     shoppingCarts.push(product);
     cartsNumber.textContent = shoppingCarts.length;
     localStorage.setItem("carts", JSON.stringify(shoppingCarts));
@@ -104,6 +119,10 @@ class Shopping {
 document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
   cartsNumber.textContent = shoppingCarts.length;
-  const shoppingApp = new Shopping(shoppingDB);
+  const shoppingApp = new Shopping();
   shoppingApp.renderProducts();
 });
+
+openModalBtn.addEventListener("click", openModal);
+backdrop.addEventListener("click", closeModal);
+modal.addEventListener("click", (e) => e.stopPropagation());
