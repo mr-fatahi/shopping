92967ba1b449578cca830e12fc4e6102afc1ed6a
Alireza Fatahi <afatahi201580@gmail.com>
2025-02-15 21:36:06 +0330

    shopping project is done...

--- js/app.js
+++ js/app.js
@@ -1,80 +1,90 @@
 let shoppingCarts = JSON.parse(localStorage.getItem("carts")) || [];
 let cartsResult;
 let fillModal;
+let fillModalBind;
+let rendering;
+let bindRendering;
+let totalPrice;
+
 const cartsNumber = document.querySelector(".number");
 const carts = document.querySelector(".carts");
+const footPrice = document.querySelector(".foot-price");
+const clearAll = document.querySelector(".clear");
 const openModalBtn = document.querySelector(".header__cart");
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const modalBody = document.querySelector(".modal__body");
+
 function openModal() {
   backdrop.classList.remove("hidden");
-  fillModal();
+  totalPrice = shoppingCarts.reduce((acc, curr) => acc + curr.totalPrice, 0);
+  footPrice.textContent = totalPrice.toLocaleString();
+  fillModalBind();
 }
 function closeModal() {
   backdrop.classList.add("hidden");
+}
+function clearCarts() {
+  shoppingCarts = [];
+  cartsNumber.textContent = shoppingCarts.length;
+  totalPrice = shoppingCarts.reduce((acc, curr) => acc + curr.totalPrice, 0);
+  footPrice.textContent = totalPrice.toLocaleString();
+  fillModalBind();
+  bindRendering();
+  localStorage.setItem("carts", JSON.stringify(shoppingCarts));
 }
 class Shopping {
   #shoppingDB = [
     {
       id: 1,
-      number: 1,
       name: "سرویس خواب دو نفره آرتا",
       price: 3_290_000,
       imageUrl: "/public/images/arta-s-two-3290.jpg",
     },
     {
       id: 2,
-      number: 1,
       name: "تخت خواب دو نفره فلورا",
       price: 4_090_000,
       imageUrl: "/public/images/flora-t-two-4090.jpg",
     },
     {
       id: 3,
-      number: 1,
       name: "تخت خواب دو طبقه رادو",
       price: 5_290_000,
       imageUrl: "/public/images/rado-2t-5290.jpg",
     },
     {
       id: 4,
-      number: 1,
       name: "سرویس خواب دو نفره راموناس",
       price: 23_200_000,
       imageUrl: "/public/images/ramonas-s-two-23200.jpg",
     },
     {
       id: 5,
-      number: 1,
       name: "سرویس خواب دو نفره روما",
       price: 2_890_000,
       imageUrl: "/public/images/rooma-s-two-2890.jpg",
     },
     {
       id: 6,
-      number: 1,
       name: "تخت خواب یک نفره رویا",
       price: 2_850_000,
       imageUrl: "/public/images/roya-t-one-2850.jpg",
     },
     {
       id: 7,
-      number: 1,
       name: "تخت خواب یک نفره صبا",
       price: 1_845_000,
       imageUrl: "/public/images/saba-t-one-1845.jpg",
     },
     {
       id: 8,
-      number: 1,
       name: "تخت خواب دو نفره سناتور",
       price: 2_950_000,
       imageUrl: "/public/images/senator-t-two-2950.jpg",
     },
     {
       id: 9,
-      number: 1,
       name: "سرویس خواب دو نفره ویدا",
       price: 3_800_000,
       imageUrl: "/public/images/vida-s-two-3800.jpg",
@@ -82,6 +92,8 @@ class Shopping {
   ];
   #result = "";
   renderProducts() {
+    carts.innerHTML = "";
+    this.#result = "";
     this.#shoppingDB.forEach((product) => {
       this.#result += `<section class="cart">
           <figure class="cart__image">
@@ -123,7 +135,12 @@ class Shopping {
     });
   }
   #addToCarts(product) {
-    shoppingCarts.push(product);
+    const addedProduct = {
+      ...product,
+      number: 1,
+      totalPrice: product.price,
+    };
+    shoppingCarts.push(addedProduct);
     cartsNumber.textContent = shoppingCarts.length;
     localStorage.setItem("carts", JSON.stringify(shoppingCarts));
   }
@@ -137,19 +154,70 @@ class Shopping {
               </div>
               <div class="modal__cart__info">
                 <p>${product.name}</p>
-                <span>${product.price.toLocaleString()} تومان</span>
+                <div>
+                  <span data-id=${
+                    product.id
+                  } class="price">${product.totalPrice.toLocaleString()}</span>
+                  <span>تومان</span>
+                </div>
               </div>
               <div class="modal__cart__numbers">
-                <i class="fa-solid fa-chevron-up"></i>
-                <span class="num">${product.number}</span>
-                <i class="fa-solid fa-chevron-down"></i>
+                <i data-id=${product.id} class="fa-solid fa-chevron-up inc"></i>
+                <span data-id=${product.id} class="num">${product.number}</span>
+                <i data-id=${
+                  product.id
+                } class="fa-solid fa-chevron-down dec"></i>
               </div>
-              <span data-id=${
-                product.id
-              } class="trash"><i class="fa-solid fa-trash-can"></i></span>
+              <i data-id=${product.id} class="fa-solid fa-trash-can trash"></i>
             </div>`;
     });
     modalBody.innerHTML = cartsResult;
+    const incBtns = [...document.querySelectorAll(".fa-chevron-up")];
+    incBtns.forEach((btn) =>
+      btn.addEventListener("click", this.#calculateProduct)
+    );
+    const decBtns = [...document.querySelectorAll(".fa-chevron-down")];
+    decBtns.forEach((btn) =>
+      btn.addEventListener("click", this.#calculateProduct)
+    );
+    const delBtns = [...document.querySelectorAll(".trash")];
+    delBtns.forEach((btn) =>
+      btn.addEventListener("click", this.#deleteFromCart)
+    );
+  }
+  #calculateProduct(e) {
+    const id = Number(e.target.dataset.id);
+    const nums = [...document.querySelectorAll(".num")];
+    const targetNum = nums.find((num) => Number(num.dataset.id) === id);
+    const prices = [...document.querySelectorAll(".price")];
+    const targetPrice = prices.find((price) => Number(price.dataset.id) === id);
+    const product = shoppingCarts.find((p) => p.id === id);
+    if ([...e.target.classList].includes("inc")) {
+      product.number++;
+    }
+    if ([...e.target.classList].includes("dec")) {
+      if (product.number === 1) {
+        e.target.disabled = true;
+      } else {
+        product.number--;
+      }
+    }
+    product.totalPrice = product.price * product.number;
+    targetNum.textContent = product.number;
+    targetPrice.textContent = product.totalPrice.toLocaleString();
+    totalPrice = shoppingCarts.reduce((acc, curr) => acc + curr.totalPrice, 0);
+    footPrice.textContent = totalPrice.toLocaleString();
+    localStorage.setItem("carts", JSON.stringify(shoppingCarts));
+  }
+  #deleteFromCart(e) {
+    const id = Number(e.target.dataset.id);
+    shoppingCarts = shoppingCarts.filter((c) => c.id !== id);
+    cartsNumber.textContent = shoppingCarts.length;
+    totalPrice = shoppingCarts.reduce((acc, curr) => acc + curr.totalPrice, 0);
+    footPrice.textContent = totalPrice.toLocaleString();
+    fillModalBind();
+    bindRendering();
+    localStorage.setItem("carts", JSON.stringify(shoppingCarts));
   }
 }
 
@@ -158,9 +226,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
   cartsNumber.textContent = shoppingCarts.length;
   const shoppingApp = new Shopping();
   shoppingApp.renderProducts();
+  rendering = shoppingApp.renderProducts;
+  bindRendering = rendering.bind(shoppingApp);
   fillModal = shoppingApp.cartsInModal;
+  fillModalBind = fillModal.bind(shoppingApp);
 });
 
 openModalBtn.addEventListener("click", openModal);
 backdrop.addEventListener("click", closeModal);
 modal.addEventListener("click", (e) => e.stopPropagation());
+clearAll.addEventListener("click", clearCarts);
